b854845ea3604ce4d389ffec0f164861a9c7017d
patrick hrabos <phrabos@gmail.com>
2021-03-01 17:50:14 -0500

    create two users with auth route, and get user data behind api auth middleware

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'code challenge',
+    completed: false,
+  },
+  {
+    todo: 'meetup',
+    completed: false,
+  },
+  {
+    todo: 'retro lab 12',
+    completed: true,
+  }
+];
